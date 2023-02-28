208edca85ef1d66dc6e28d2ce02d8d3418066423
Camilo Figueroa <cigotete@gmail.com>
2023-02-28 12:54:22 -0500

    Created table Profile using migration executed.

---
+++ database/migrations/2023_02_28_164328_create_profiles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 128);
+            $table->text('biography');
+            $table->string('website', 128);
+
+            // Foreign Key to uid at users table.
+            $table->unsignedBigInteger('user_id')
+            ->unique();
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
