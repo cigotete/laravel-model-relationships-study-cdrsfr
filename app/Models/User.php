3648b91dda0f082dc7e89e9fd31e0c1b8c578ebb
Camilo Figueroa <cigotete@gmail.com>
2023-02-28 19:32:49 -0500

    Created migrations and models for Post and Categories. Also updated User model to relate it to Post model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,8 @@ class User extends Authenticatable
         //return $profile = Profile::where('user_id', $this->id)->first();
         return $this->hasOne(Profile::class);
     }
+
+    public function posts() {
+        return $this->hasMany(Post::class);
+    }
 }
