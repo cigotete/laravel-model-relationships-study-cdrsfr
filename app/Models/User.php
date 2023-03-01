065aaebc7c7000f7a8b2f562190106b58d57c814
Camilo Figueroa <cigotete@gmail.com>
2023-03-01 12:23:30 -0500

    Created Video model and related it with User model and users table.

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,8 @@ class User extends Authenticatable
     public function posts() {
         return $this->hasMany(Post::class);
     }
+
+    public function videos() {
+        return $this->hasMany(Video::class);
+    }
 }
