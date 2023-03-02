55b481a984d3ef5264b10e8fb7f1df4f58f39c49
Camilo Figueroa <cigotete@gmail.com>
2023-03-01 21:17:24 -0500

    Added Polymorphic relation One to Many among Comment model and Post and Videos models.

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -9,7 +9,13 @@ class Video extends Model
 {
     use HasFactory;
 
+    protected $guarded = [];
+
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+    public function comments() {
+        return $this->morphMany(Comment::class, 'commentable');
     }
 }
